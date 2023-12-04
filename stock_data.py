5b3d8144e9a10d6f17ea67f4b09795711bcaabf8
VishnuGops <vishvargops@gmail.com>
2023-12-04 01:17:53 -0800

    sql integration

--- stock_data.py
+++ stock_data.py
@@ -1,32 +1,39 @@
-# IMPORT THE LIBRARY
 import yfinance as yf
-from datetime import datetime
-import finplot as fplt
 
 
-def get_data():
-    # CREATE TICKER INSTANCE FOR AMAZON
-    amzn = yf.Ticker("TSLA")
+def get_stock_data(ticker):
+    # Fetch stock data from Yahoo Finance
+    stock_data = yf.download(ticker, period="5d", interval="1d")
 
-    # GET TODAYS DATE AND CONVERT IT TO A STRING WITH YYYY-MM-DD FORMAT (YFINANCE EXPECTS THAT FORMAT)
-    end_date = datetime.now().strftime('%Y-%m-%d')
-    amzn_hist = amzn.history(start='2018-01-01', end=end_date)
-    print(amzn_hist)
+    # Calculate some basic statistics
+    average_price = stock_data['Close'].mean()
+    standard_deviation = stock_data['Close'].std()
+    moving_average = stock_data['Close'].rolling(window=20).mean()
+    macd = stock_data['Close'].ewm(span=12, min_periods=9).mean(
+    ) - stock_data['Close'].ewm(span=26, min_periods=25).mean()
+    rsi = stock_data['Close'].diff(1).ewm(span=14).mean(
+    ) / stock_data['Close'].diff(1).ewm(span=14, min_periods=13).mean() * 100
 
+    # Generate a summary of the stock data
+    summary = {
+        "Ticker": ticker,
+        "Average Price": average_price,
+        "Standard Deviation": standard_deviation,
+        "Moving Average (20-day)": moving_average.iloc[-1],
+        "MACD": macd.iloc[-1],
+        "RSI": rsi.iloc[-1]
+    }
 
-def plot_graph(ticker):
-    # RETRIEVE 1 YEAR WORTH OF DAILY DATA OF TESLA
-    end_date = datetime.now().strftime('%Y-%m-%d')
-    df = ticker.history(interval='1d', period='1y')
-
-    # PLOT THE OHLC CANDLE CHART
-    fplt.candlestick_ochl(df[['Open', 'Close', 'High', 'Low']])
-    fplt.show()
+    return summary
 
 
 def main():
-    get_data()
-    plot_graph(yf.Ticker('SPY'))
+    ticker = input("Enter a stock ticker: ")
+    stock_data = get_stock_data(ticker)
+
+    print("Stock Summary:")
+    for key, value in stock_data.items():
+        print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
